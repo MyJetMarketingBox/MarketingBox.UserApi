c0d8436020cd21fa5b8096a78f07de792fcf2f76
Lev Kurbanov <lev.k@smplt.net>
2022-05-23 14:45:36 +0500

    fix docker file, fix validation

--- src/MarketingBox.UserApi/Models/ResetPasswordRequestHttp.cs
+++ src/MarketingBox.UserApi/Models/ResetPasswordRequestHttp.cs
@@ -1,14 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using MarketingBox.Sdk.Common.Attributes;
 using MarketingBox.Sdk.Common.Models;
 
 namespace MarketingBox.UserApi.Models
 {
     public class ResetPasswordRequestHttp : ValidatableEntity
     {
-        [Required, StringLength(128,MinimumLength = 1)]
+        [Required, IsValidPassword]
         public string OldPassword { get; set; }
         
-        [Required, StringLength(128,MinimumLength = 1)]
+        [Required, IsValidPassword]
         public string NewPassword { get; set; }
     }
 }
